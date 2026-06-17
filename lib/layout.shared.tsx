489c20edab04c60138380c902dd70f5b26c4dff2
Balkis <balqisdemespiscesa@gmail.com>
2026-06-17 15:53:50 +0700

    add getting started dan akun

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -1,11 +1,20 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
-import { appName, gitConfig } from './shared';
+import Image from 'next/image';
+import { gitConfig } from './shared';
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      // JSX supported
-      title: appName,
+      title: (
+        <Image
+          src="/assets/gameforsmartlogo.png"
+          alt="GameForSmart Docs"
+          width={140}
+          height={36}
+          style={{ objectFit: 'contain' }}
+          priority
+        />
+      ),
     },
     githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
   };
